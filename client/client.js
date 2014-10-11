db5ca83f50594477ba3b6a2b911b885cac47f5ed
Dead-i <ethan.waite@btinternet.com>
2014-10-11 15:35:18 +0100

    Chat toggling

--- client/client.js
+++ client/client.js
@@ -6,11 +6,19 @@ var loc = 'http://localhost:8080';
 
 // Create the frame
 $('body').append('<iframe src="about:blank" id="reachirc" scrolling="no"></iframe>');
-$('#reachirc').css({ height: '50px', border: 'none', position: 'fixed', bottom: '0px', right: '12px' });
+$('#reachirc').css({ 'height': 50, 'border': 'none', 'z-index': 900, 'position': 'fixed', 'bottom': 0, 'right': 24 });
+
+// Get the frame's contents
+var c = $('#reachirc').contents();
 
 // Add the CSS file
-$('#reachirc').contents().find('head').html('<link href="' + loc + '/style.css" rel="stylesheet" type="text/css" />');
+c.find('head').html('<link href="' + loc + '/style.css" rel="stylesheet" type="text/css" />');
 
 // Main bar
-var body = $('#reachirc').contents().find('body');
-body.html('<div class="chatbar">Chat with us on IRC</div>');
+var b = $('#reachirc').contents().find('body');
+b.html('<div class="main"><div class="title">Chat with us on IRC</div><div class="content"></div></div>');
+
+// When the bar is clicked
+$('.title', b).click(function() {
+	$('#reachirc').toggleClass('active').animate({ height: $('#reachirc').hasClass('active') ? 300 : 50 }, 500);
+});
